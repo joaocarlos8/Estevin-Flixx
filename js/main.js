b5f60be0ada2672370463d2d94978af3157a1b67
enegreeli <estevaogg123@gmail.com>
2025-03-20 20:30:18 -0300

    finalizado js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
+
+//API key
+const options = {
+    method: 'GET', 
+    headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjZDljYTA5YTNjMmMyNTY3ZjMzNmU3OGQ2MjI5MjQwMyIsIm5iZiI6MTc0MjUxMjI1MS43ODMsInN1YiI6IjY3ZGNhMDdiMzUwODkxMTQ1MjZiYTlkNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MPm0dGLGnh4q7e2cfwLMFhk04BzswCTs4-DuJqDFbo8'
+    }
+}
+
 // Exibir loading 
 function toggleLoading() { 
     let loader = document.querySelector(".loader"); 
@@ -13,3 +23,24 @@ window.addEventListener("scroll", function () {
         navbar.classList.remove("scrolled"); 
     } 
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Key api my eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjZDljYTA5YTNjMmMyNTY3ZjMzNmU3OGQ2MjI5MjQwMyIsIm5iZiI6MTc0MjUxMjI1MS43ODMsInN1YiI6IjY3ZGNhMDdiMzUwODkxMTQ1MjZiYTlkNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MPm0dGLGnh4q7e2cfwLMFhk04BzswCTs4-DuJqDFbo8
